d00ddd2549a3e2a9b8c169096fdeed949c5113cc
LeoOlivaresD <cspraword@gmail.com>
2023-04-26 22:55:02 -0400

    main

---
+++ src/main/java/cl/leo/restapiyoutube/RestApiYoutubeApplication.java
@@ -0,0 +1,13 @@
+package cl.leo.restapiyoutube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiYoutubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiYoutubeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/leo/restapiyoutube/RestApiYoutubeApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.leo.restapiyoutube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiYoutubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
